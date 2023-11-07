084f707baabce1dd1d685d14b4777ded0f5b1571
NandithaNK <nandnk214@gmail.com>
2023-11-07 13:57:20 +0530

    Completed Task 4

--- app.py
+++ app.py
@@ -1,31 +1,67 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc
+import dash
+from dash import html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-app = Dash(__name__)
+# Initializing Dash application
+app = dash.Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+# Reading data from CSV file
 df = pd.read_csv("Quantium Task 2.csv")
 
-fig = px.line(df, x='date', y='Sales', title='Sales Over Time')
+# Extracting unique regions from the data and converting them to a list called regions.
+regions = df['region'].unique().tolist()
+# this is how all the regions are detected.
+# Inserting "All" option at the top of the regions radio buttons
+regions.insert(0, "All")
 
-app.layout = html.Div(children=[
-    html.H1(children='Price of Pink Morsel Over Time'),
+# Setting default region to "All"
+default_region = regions[0]
 
-    html.Div(children='''
-        Line graph shows Date vs Price of Pink Morsel.
-             Sales are higher after the price was increased.
-    '''),
+# Creating initial line graph
+fig = px.line(df, x="date", y="Sales", title=f"Sales Over Time - {default_region}")
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Defining Dash app layout
+app.layout = html.Div([
+    html.H1(children="Sales of Pink Morsel Over Time", style={"padding": "20px", "color": "White"}),
+    html.Div("Choose the region you want the sales data of:", style={"background-color": "#2C5D63", "color": "White", "font-size": "20px", "padding": "20px"}),
+
+    # Radio button component with inline CSS
+    dcc.RadioItems(
+        id="region-radio",
+        options=[{"label": region, "value": region} for region in regions],
+        value=default_region,
+        labelStyle={"display": "block"},
+        style={"font-size": "18px", "background-color": "#2C5D63", "color": "White", "padding": "20px"},
+    ),
+
+    # Graph component to display the sales data
+    dcc.Graph(id="example-graph", figure=fig),
+], style={"height": "100%", "width": "100%", "position": "absolute", "background-color": "#283739"})
+
+# Define callback function to update the graph based on selected region
+@app.callback(
+    Output("example-graph", "figure"),
+    [Input("region-radio", "value")],
+)
+def update_graph(selected_region):
+    # Filter data based on selected region
+    df_filtered = df if selected_region == "All" else df[df["region"] == selected_region]
+
+    # Update graph title based on selected region
+    title = "Sales Over Time - All Regions" if selected_region == "All" else f"Sales Over Time - {selected_region}"
+
+    # Update graph figure with filtered data and updated title
+    updated_fig = px.line(df_filtered, x="date", y="Sales", title=title)
+
+    # Apply custom background color to the area around the line
+    updated_fig.update_layout(paper_bgcolor="#A9C52F")
+
+    # Maintain original background colors for the graph area
+    updated_fig.update_layout(plot_bgcolor="#F7EEBB")
+
+    return updated_fig
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
